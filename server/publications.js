47759d396fc0746eb1da5d50313ae99acecdbdc0
pengcl <3559996@qq.com>
2016-03-10 18:00:31 +0800

    change staff

--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,30 @@
-Meteor.publish('posts',function(){
+Meteor.publish('posts', function () {
     return Posts.find();
+});
+
+Meteor.publish('post', function (id) {
+    check(id, String);
+    return [
+    BookmarkCounts.find({
+            postId: id
+        }),
+    Activities.find({
+            postId: id
+        })
+  ];
+});
+
+Meteor.publish('bookmarkCounts', function () {
+    return BookmarkCounts.find();
+});
+
+// autopublish the user's bookmarks and admin status
+Meteor.publish(null, function () {
+    return Meteor.users.find(this.userId, {
+        fields: {
+            admin: 1,
+            bookmarkedPostIds: 1,
+            'services.twitter.profile_image_url_https': 1
+        }
+    });
 });
