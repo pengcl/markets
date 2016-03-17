75b802aaf172ef67f453073c4c9f11b8bed4089a
pengcl <3559996@qq.com>
2016-03-17 10:18:12 +0800

    dfdfg

--- client/templates/wxApi/wxApi.js
+++ client/templates/wxApi/wxApi.js
@@ -1,35 +1,23 @@
-Template.wxApi.helpers({
-    TOKEN:'weixin',
-    /*posts: function () {
-        return Posts.find({}, {
-            sort: {
-                submitted: -1
-            }
-        });
-    }*/
-});
+var TOKEN: 'weixin';
 
-var TOKEN = "weixin";
+Template.wxApi.checkSignature = function () {
+    var signature = $_GET["signature"];
+    var timestamp = $_GET["timestamp"];
+    var nonce = $_GET["nonce"];
 
-function checkSignature()
-{
-        var signature = $_GET["signature"];
-        var timestamp = $_GET["timestamp"];
-        var nonce = $_GET["nonce"];
+    var token = TOKEN;
+    var tmpArr = array($token, $timestamp, $nonce);
+    sort($tmpArr, SORT_STRING);
+    var tmpStr = implode($tmpArr);
+    tmpStr = sha1($tmpStr);
 
-	var token = TOKEN;
-	var tmpArr = array($token, $timestamp, $nonce);
-	sort($tmpArr, SORT_STRING);
-	var tmpStr = implode( $tmpArr );
-	tmpStr = sha1( $tmpStr );
+    if (tmpStr == signature) {
+        return true;
+    } else {
+        return false;
+    }
+};
 
-	if( tmpStr == signature ){
-		return true;
-	}else{
-		return false;
-	}
-}
-
-function wechatCallbackapiTest {
+Template.wxApi.wechatCallbackapiTest = function () {
 
 }
