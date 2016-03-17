96b7054fe46eecd2442bdf89d2573b1f9cce445e
pengcl <3559996@qq.com>
2016-03-17 18:15:37 +0800

    asdfasdf

--- client/templates/wxApi/wxApi.js
+++ client/templates/wxApi/wxApi.js
@@ -1,35 +1,43 @@
+Template.wxApi.onCreated(function () {
+    Meteor.call('getAccessToken', function (error, result) {
+        // 显示错误信息并退出
+        if (error) {
+            return throwError(error.reason);
+        }
+
+        if (result.postExists) {
+            throwError('This link has already been posted（该链接已经存在）');
+        }
+    });
+});
 Template.wxApi.helpers({
-    TOKEN:'weixin',
-    /*posts: function () {
-        return Posts.find({}, {
-            sort: {
-                submitted: -1
-            }
-        });
-    }*/
+
 });
 
-var TOKEN = "weixin";
+Template.wxApi.checkSignature = function () {
+    /*var TOKEN = "weixin";
+    var controller = Iron.controller();
+    var signature = controller.state.get('signature');
+    var timestamp = controller.state.get('timestamp');
+    var nonce = controller.state.get('nonce');
+    var echoStr = controller.state.get('echostr');
 
-function checkSignature()
-{
-        var signature = $_GET["signature"];
-        var timestamp = $_GET["timestamp"];
-        var nonce = $_GET["nonce"];
+    var token = TOKEN;
+    var tmpArr = new Array(token, timestamp, nonce);
+    sort(tmpArr);
+    var tmpStr = tmpStr.join();
+    tmpStr = sha1(tmpStr);
 
-	var token = TOKEN;
-	var tmpArr = array($token, $timestamp, $nonce);
-	sort($tmpArr, SORT_STRING);
-	var tmpStr = implode( $tmpArr );
-	tmpStr = sha1( $tmpStr );
-
-	if( tmpStr == signature ){
-		return true;
-	}else{
-		return false;
-	}
+    if (tmpStr == signature) {
+        return true;
+    } else {
+        return false;
+    }*/
 }
 
-function wechatCallbackapiTest {
-
+Template.wxApi.wechatCallbackapiTest = function () {
+    /*if(Template.wxApi.checkSignature()){
+        alert(echoStr);
+        return;
+    }*/
 }
