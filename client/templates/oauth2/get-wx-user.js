a746aeca22a243956d9c2097b27bba29318c45d3
pengcl <3559996@qq.com>
2016-03-25 17:44:23 +0800

    adsfasdf

--- client/templates/oauth2/get-wx-user.js
+++ client/templates/oauth2/get-wx-user.js
@@ -35,13 +35,3 @@ Template.getWxUser.getWxUserinfo = function (code) {
         }
     });
 };
-
-Template.getWxUser.createPassword = function () {
-    var data = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    var result = "";
-    for (var i = 0; i < 20; i++) {
-        var r = Math.floor(Math.random() * 62); //取得0-62间的随机数，目的是以此当下标取数组data里的值！
-        result += data[r]; //输出20次随机数的同时，让rrr加20次，就是20位的随机字符串了。
-    }
-    return result
-}
